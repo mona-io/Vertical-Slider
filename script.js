59ba58953da63abf46d5a81e6e1b408b102aa746
mona-io <mona.aziminia88@gmail.com>
2021-11-07 00:06:54 +0330

    changes

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ const slidesLength = rightSlide.querySelectorAll ('div').length
 let activeSlideIndex = 0
 
 leftSlide.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight
+  if (direction === 'up') {
+    activeSliderIndex ++
+    if (activeSlideIndex > slideLength -1) {
+    activeSliderIndex = 0
+    }
+  }
+
+   
+}
