15f2b1f02536a84f28d13c59199434083bc91b24
mona-io <mona.aziminia88@gmail.com>
2021-11-08 21:22:13 +0330

    more changes

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-const sliderContainer = document.querySelector ('.slider-container')
-const leftSlide = document.querySelector ('.left-slide')
-const rightSlide = document.querySelector ('.right-slide')
-const upButton = document.querySelector ('.up-button')
-const downButton = document.querySelector ('.down-button')
-const slidesLength = rightSlide.querySelectorAll ('div').length
+const sliderContainer = document.querySelector('.slider-container')
+const leftSlide = document.querySelector('.left-slide')
+const rightSlide = document.querySelector('.right-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slideLength = rightSlide.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
-leftSlide.style.top = `-${(slidesLength - 1) * 100}vh`
+leftSlide.style.top = `-${(slideLength - 1) * 100}vh`
 
 upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
-
 const changeSlide = (direction) => {
   const sliderHeight = sliderContainer.clientHeight
-  if (direction === 'up') {
-    activeSliderIndex ++
-    if (activeSlideIndex > slideLength -1) {
-    activeSliderIndex = 0
+  if(direction === 'up') {
+    activeSlideIndex++
+    if(activeSlideIndex > slideLength - 1) {
+      activeSlideIndex = 0
+    } 
+  } else if(direction === 'down') {
+      activeSlideIndex--
+      if(activeSlideIndex < 0) {
+        activeSlideIndex = slideLength - 1
+      }
     }
-  }
 
-   
+  rightSlide.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+  leftSlide.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
